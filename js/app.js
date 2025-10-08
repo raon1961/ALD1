cc50e75a76765949d029562a1578c2c95bcb1216
raon1961 <luou0819@gmail.com>
2025-10-08 19:55:26 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -139,16 +139,16 @@ function SocialSection() {
   return React.createElement(
     "div",
     { ref: ref, className: `mt-12 flex justify-center space-x-6 opacity-0 transform translate-y-10 transition duration-700 ${visible ? "opacity-100 translate-y-0" : ""}` },
-    React.createElement("a", { href: "https://m.youtube.com/@f1are11_11", target: "_blank", rel: "noopener noreferrer" },
+    React.createElement("a", { href: "https://www.youtube.com/@ALD1.official", target: "_blank", rel: "noopener noreferrer" },
       React.createElement("img", { src: "images/youtube.png", alt: "YouTube", className: "w-8 h-8 sm:w-10 sm:h-10 hover:scale-125 transition transform duration-300" })
     ),
-    React.createElement("a", { href: "https://www.instagram.com/f1are11_official", target: "_blank", rel: "noopener noreferrer" },
+    React.createElement("a", { href: "https://www.instagram.com/ald1.official", target: "_blank", rel: "noopener noreferrer" },
       React.createElement("img", { src: "images/instagram.png", alt: "Instagram", className: "w-8 h-8 sm:w-10 sm:h-10 hover:scale-125 transition transform duration-300" })
     ),
-    React.createElement("a", { href: "https://x.com/standbyf1are?s=21", target: "_blank", rel: "noopener noreferrer" },
+    React.createElement("a", { href: "https://x.com/ALD1_official", target: "_blank", rel: "noopener noreferrer" },
       React.createElement("img", { src: "images/x.png", alt: "X", className: "w-8 h-8 sm:w-10 sm:h-10 hover:scale-125 transition transform duration-300" })
     ),
-    React.createElement("a", { href: "https://www.tiktok.com/@f1are1171", target: "_blank", rel: "noopener noreferrer" },
+    React.createElement("a", { href: "https://www.tiktok.com/@ald1_official", target: "_blank", rel: "noopener noreferrer" },
       React.createElement("img", { src: "images/TikTok.png", alt: "TikTok", className: "w-8 h-8 sm:w-10 sm:h-10 hover:scale-125 transition transform duration-300" })
     )
   );
